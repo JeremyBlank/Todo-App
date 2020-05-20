2d876b81a509b784348e10464c73f1df1d523b34
Jeremy Blank <jeremy.r.blank@gmail.com>
2020-05-20 15:42:06 +0200

    added

---
+++ CreaterReadme.md
@@ -0,0 +1,44 @@
++  Firebase initialization complete!
+PS C:\Users\jrbla\OneDrive\Desktop\Todo-App> npm run build
+
+> todo-app@0.1.0 build C:\Users\jrbla\OneDrive\Desktop\Todo-App
+> react-scripts build
+
+Creating an optimized production build...
+Compiled successfully.
+
+File sizes after gzip:
+  260.84 KB  build\static\js\2.1b0be1da.chunk.js
+  951 B      build\static\js\main.c8d80b8f.chunk.js
+  776 B      build\static\js\runtime-main.1b47fdd3.js
+  72 B       build\static\css\main.5facb584.chunk.css
+
+The project was built assuming it is hosted at /.
+You can control this with the homepage field in your package.json.
+
+The build folder is ready to be deployed.
+You may serve it with a static server:
+
+  npm install -g serve
+  serve -s build
+
+Find out more about deployment here:
+  bit.ly/CRA-deploy
+
+PS C:\Users\jrbla\OneDrive\Desktop\Todo-App> npm install -g server
+npm WARN deprecated core-js@2.6.11: core-js@<3 is no longer maintained and not recommended for usage due to the number of issues. Please, upgrade your dependencies to the actual version of core-js@3.                                                                                                               ndencies to the actual version of core-js@3.  
+
+> core-js@2.6.11 postinstall C:\Users\jrbla\AppData\Roaming\npm\node_modules\server\node_modules\core-js
+> node -e "try{require('./postinstall')}catch(e){}"
+
+Thank you for using core-js ( https://github.com/zloirock/core-js ) for polyfilling JavaScript standard library!
+
+The project needs your help! Please consider supporting of core-js on Open Collective or Patreon:
+> https://opencollective.com/core-js
+> https://www.patreon.com/zloirock
+
+Also, the author of core-js ( https://github.com/zloirock ) is looking for a good job -)
+
++ server@1.0.27
+added 233 packages from 218 contributors in 41.659s
+PS C:\Users\jrbla\OneDrive\Desktop\Todo-App>
--- src/App.js
+++ src/App.js
@@ -1,38 +1,65 @@
-import React, { useState } from "react";
+import React, {
+  useState,
+  useEffect
+}
+from "react";
 import "./App.css";
 import Todo from "./Todo";
+import db from './firebase';
 
 function App() {
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
 
+  useEffect(() => {
+
+    db.collection('todos').onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map((doc) => doc.data().title))
+    });
+  }, []);
+
   const addTodo = (e) => {
     e.preventDefault();
-    setTodos([...todos, input]);  // add whatever input to to todos array
-    setInput("");  // clear the input field
+    // setTodos([...todos, input]); // add whatever input to to todos array
+    setInput(""); // clear the input field
 
+    db.collection('todos').add({
+      title: input,
+    })
   };
 
   return (
-    <div className="App">
+    <div className = "App">
       <h1>Todo App</h1>
 
-      <form>
+      <form >
         <input
           value={input}
-          onChange={(e) => setInput(e.target.value)}
-          type="text"
-        />
-        <button type="submit" onClick={addTodo}>
-          Add todo
-        </button>
+          onChange = {(e) => setInput(e.target.value)}
+          type = "text"/>
+          <button type = "submit"onClick = {addTodo} > Add todo </button> 
       </form>
 
-      {todos.map((todo) => (
-        <Todo title={todo} />
-      ))}
+      {todos.map((todo, i) => ( <Todo title={todo}key={i}/>))} 
     </div>
   );
 }
 
 export default App;
+
+
+// git add .
+// get status
+// git commit -m 'what you did'
+// git push
+// npm install --save firebase
+// npm install -g firebase-tools (frm cmd)
+// if access denied add sudo in the beginning
+// firebase init
+// go to hosting and hit spacebar then enter
+// use existing project
+// what do you want to use as your public directory? build
+// y
+// npm run build
+// firebase deploy
+// npm run deploy
